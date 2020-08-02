fb80d3a7df04aa50e532b3885c0d104d5616d63c
Dave Cook <DaveCook@hotmail.co.uk>
2020-08-02 17:19:41 +0700

    added success/failure to RC

--- Shape.js
+++ Shape.js
@@ -1,16 +1,44 @@
-const RC = require('./validation');
+const RC = require("./validation");
 
 class Shape {
-  constructor(){
+  constructor() {
     const CI = this;
-    CI.type = 'shape';
+    CI.type = "shape";
     CI.length = 10;
-    CI.color = '#000000';
-
+    CI.color = "#000000";
   }
 
-  initialize(){
+  initialize(attributes) {
+    const call_result = {};
+    const debug_data = [];
+    const return_msg = "example_code:shape:initialize ";
+    const task_id = "example_code:shape:initialize";
+    const CI = this;
 
+    // validate inputs
+    const attributesIsObject = RC.is_object(attributes);
+    if (!attributesIsObject) {
+      return_msg += "attributes must be an object";
+      return {
+        success: RC.failure,
+        return_msg: return_msg,
+        debug_data: debug_data,
+      };
+    }
+
+    if (attributes.hasOwnProperty(length)){
+      const length_is_number = RC.is_number(attributes.length);
+      CI.set_length(attributes.length);
+    }
+    if (attributes.hasOwnProperty(length)){
+      CI.set_length(length);
+    }
+
+    return {
+      success: RC.success,
+      return_msg: return_msg,
+      debug_data: debug_data,
+    };
   }
 }
 
--- validation.js
+++ validation.js
@@ -1,13 +1,35 @@
 const RC = {
-  is_string: (str) => isString(str),
+  is_string: function(str) {
+    if (isString(str)){
+      return this.success;
+    } else {
+      return this.failure;
+    }
+  },
   is_hex_color: function (str) {
     if (this.is_string(str)){
-      return str.match(/^#(?:[0-9a-f]{3}){1,2}$/i) !== undefined;
+      if (str.match(/^#(?:[0-9a-f]{3}){1,2}$/i) !== undefined) {
+        return this.success;
+      }
     }
-    return false;
+    return this.failure;
   },
-  is_number: (n) => isNumber(n),
-
+  is_number: function(n){
+    if(isNumber(n)){
+      return this.success;
+    } else {
+      return this.failure;
+    }
+  },
+  is_object: function(o){
+    if (isObject(o)){
+      return this.success;
+    } else {
+      return this.failure;
+    }
+  },
+  success:'success',
+  failure:'failure'
 }
 
 module.exports = RC;
